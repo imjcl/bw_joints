ac958f0ddf5587554ede2e92785c6f9c1b6a06c9
James Liu <jaycliu@gmail.com>
2014-07-23 17:35:14 -0700

    change some styles and structure of the footer

--- footer.php
+++ footer.php
@@ -8,8 +8,10 @@
 		    					</nav>
 		    				</div>
 			               
-			                <div class="large-12 medium-12 columns">		
-								<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+              <div class="large-12 medium-12 columns">
+								<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+
+								<p>Follow us!</p>
 							</div>		
 						</div> <!-- end #inner-footer -->			
 					</footer> <!-- end .footer -->
