94042455497cc88b4eb6682d1589912ebe7fab0b
James Liu <jaycliu@gmail.com>
2014-07-24 17:31:31 -0700

    create some placeholders

--- footer.php
+++ footer.php
@@ -7,11 +7,37 @@
 		    						<?php joints_footer_links(); ?>
 		    					</nav>
 		    				</div>
-			               
+
+							<div class="large-3 medium-3 small-12 columns">
+								<h3>About</h3>
+								<p>Our Instructors<br>
+								Classes<br>
+								Schedule<br>
+								Blog</p>
+							</div>	
+
+							<div class="large-3 medium-3 small-12 columns">
+								<h3>Follow Us</h3>
+								<ul>
+									<li>twitter</li>
+									<li>facebook</li>
+									<li>instagram</li>
+								</ul>
+							</div>					              				              							
+							<div class="large-3 medium-3 small-12 columns">
+							</div>					              
+
+							<div class="large-3 medium-3 small-12 columns">
+								<h3>Contact</h3>
+								<p><strong>Boxing Works</strong><br>
+								307 Pacific Coast Highway<br>
+								Hermosa Beach, CA 90254<br>
+								(310) 376-1602
+								</p>
+							</div>					              
+
               <div class="large-12 medium-12 columns">
 								<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
-
-								<p>Follow us!</p>
 							</div>		
 						</div> <!-- end #inner-footer -->			
 					</footer> <!-- end .footer -->
